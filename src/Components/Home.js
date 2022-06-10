b0c0f5ca4c47c4ce25cdeca09152e07e97c444dd
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-06-10 17:19:14 +0530

    1 file modified

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,10 @@ import React from 'react';
 const Home = () => {
   return (
     <main>
-      <button className="sidebar-toggle"></button>
+      <button className="sidebar-toggle">
+        <i class="fa-solid fa-align-left"></i>
+      </button>
+      <button className="btn">Modal</button>
     </main>
   );
 };
