d43413064434a60cb9b003c48ee886a8eb674484
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-06-10 17:11:46 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './style.css';
+import Home from './Components/Home.js';
 import Model from './Components/model.js';
 import Sidebar from './Components/sidebar.js';
 export default function App() {
   return (
     <div>
+      <Home/>
       <Model />
       <Sidebar />
     </div>
